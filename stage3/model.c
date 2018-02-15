47c6c0344153df52949ea330a7c4b7a4c0db71f2
dsjackins <danieljackins@gmail.com>
2018-02-14 22:04:38 -0800

    updated collision
    fixed some collision logic and added paddle collision. will have to redo brick collision detection to be more efficient.

--- stage3/model.c
+++ stage3/model.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include "model.h"
 
-void move_ball(Ball *ball, Brick *bricks)
+void move_ball(Ball *ball, Brick *bricks, Paddle *paddle)
 {
         ball->x += ball->x_speed * ball->x_direction;
         ball->y += ball->y_speed * ball->y_direction;
+        
+        if (ball_collides(ball, bricks, paddle))
+        {
+                move_ball(ball, bricks, paddle);         /* collision occurred, this will move ball back 
+                                                            with direction reversed */
+        }
 }
 
 bool ball_collides(Ball *ball, Brick *bricks, Paddle *paddle)
@@ -12,25 +18,25 @@ bool ball_collides(Ball *ball, Brick *bricks, Paddle *paddle)
     int x;
     int y;
     
-    if (ball->y  == paddle->y && paddle->x <= ball->x <= paddle->x + paddle->width)     /* collision with paddle */
+    if (ball->y  <= paddle->y && paddle->x <= ball->x <= paddle->x + paddle->width)     /* collision with paddle */
     {    
         ball->y_direction = -1;
         return True;
     }
     
-    if (ball->x == 0 || ball->x + ball->width == 640)       /* collision with side walls */
+    if (ball->x <= 0 || ball->x + ball->width >= 640)       /* collision with side walls */
     {
         ball->x_direction *= -1;
         return True;
     }
     
-    if (ball->y == 0 || ball->y + ball->height == 400)      /* collision with top/bottom walls */
+    if (ball->y <= 0 || ball->y + ball->height >= 400)      /* collision with top/bottom walls */
     {
         ball->y_direction *= -1;
         return True;
     }
     
-    if ((ball->y) <= ((bricks + y*BRICK_COLS + x)->height * BRICK_ROWS))
+    /*if ((ball->y) <= ((bricks + y*BRICK_COLS + x)->height * BRICK_ROWS))
     {
         for (x = 0; x < BRICK_ROWS; x++)
         {
@@ -38,32 +44,32 @@ bool ball_collides(Ball *ball, Brick *bricks, Paddle *paddle)
             {
                 if ((bricks + y*BRICK_COLS + x)->broken == False)
                 {
-                    if (ball->y + ball->height == (bricks + y*BRICK_COLS + x)->y)          /* collision with top of a brick */
+                    if (ball->y + ball->height == (bricks + y*BRICK_COLS + x)->y)           collision with top of a brick 
                     {
                         ball->y_direction *= -1;
                         return True;
                     }
                     
-                    if (ball->y == (bricks + y*BRICK_COLS + x)->y + (bricks + y*BRICK_COLS + x)->height)  /* collision with bottom of a brick */
+                    if (ball->y == (bricks + y*BRICK_COLS + x)->y + (bricks + y*BRICK_COLS + x)->height)  collision with bottom of a brick 
                     {
                         ball->y_direction *= -1;
                         return True;
                     }   
                     
-                    if (ball->x + ball->width == (bricks + y*BRICK_COLS + x)->x)           /* collision with left of a brick */
-                    {
+                    if (ball->x + ball->width == (bricks + y*BRICK_COLS + x)->x)            collision with left of a brick 
+                   
                         ball->x_direction *= -1;
                         return True;
                     }
                     
-                    if (ball->x == (bricks + y*BRICK_COLS + x)->x + (bricks + y*BRICK_COLS + x)->width)   /* collision with right of a brick */
+                    if (ball->x == (bricks + y*BRICK_COLS + x)->x + (bricks + y*BRICK_COLS + x)->width)    collision with right of a brick 
                     {
                         ball->x_direction *= -1;
                         return True;
                     }
                 }
             }
-        }
+        }*/
     }
     
     return False;
@@ -72,8 +78,20 @@ bool ball_collides(Ball *ball, Brick *bricks, Paddle *paddle)
 void move_paddle(Paddle *paddle)
 {
     paddle->x += paddle->speed * paddle->direction;
+    if (paddle_collides(paddle))
+        move_paddle(paddle);
 }
 
+bool paddle_collides(Paddle *paddle)
+{
+    if (paddle->x <= 0 || paddle->x + paddle->width >= 640)       /* collision with side walls */
+    {
+        paddle->x_direction *= -1;
+        return True;
+    }
+    return False;
+}
+        
 void launch_ball(Paddle *paddle, Ball *ball)
 {
     int random = rand() % 2;
