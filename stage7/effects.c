2deeca6ea72e4ffc4834b0f5e242182c3120a791
dsjackins <danieljackins@gmail.com>
2018-04-08 19:30:07 -0700

    Update effects.c

--- stage7/effects.c
+++ stage7/effects.c
@@ -20,26 +20,22 @@ Instructor: Paul Pospisil
 
 void play_bounce()
 {
-	/*
-	set_noise(0x0F);
-	enable_channel(1,0,1);
-	enable_channel(2,0,1);
-	set_volume(1,0x10);
-	set_volume(2,0x10);
-	set_envelope(0x0,0x01);
-	return;
-	*/
+    set_noise(15);
+    enable_channel(1,0,1);
+    set_volume(1,16);
+    set_envelope(0,0x1000);
+    write_psg(13,0);
+    
+    return;
 }
 
 void play_brick_destroyed()
 {
-	/*
-	set_noise(0x1F);
-	enable_channel(1,0,1);
-	enable_channel(2,0,1);
-	set_volume(1,0x10);
-	set_volume(2,0x10);
-	set_envelope(0x0,0x01);
-	return;
-	*/
+    set_noise(0x1F);
+    set_envelope(0,0x3800);
+    enable_channel(2,0,1);
+    set_volume(2,16);
+    write_psg(13,0);
+	
+    return;
 }
